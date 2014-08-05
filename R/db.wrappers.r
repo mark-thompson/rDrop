47b41c2bbaec744f351c6644c735dbc8139a4ebf
mark-thompson <mthompson@2u.com>
2014-08-05 18:10:17 +0000

    added db.get.xlsx to db.wrappers

--- R/db.wrappers.r
+++ R/db.wrappers.r
@@ -20,3 +20,28 @@ db.read.csv <- function(dropbox_credentials, file_to_get, ...)
     
     return(dat)
 }
+
+
+#' Save a local copy of XLSX file from Dropbox
+#'
+#' This function is a simple wrapper around \code{\link{dropbox_get}} which writes raw bytes to a local xlsx file as specified by file_to_write
+#' @param dropbox_credentials Specifies an object of class DropboxCredentials with Dropobox specific credentials.
+#' @param  file_to_get Specifies the path to the file you want to retrieve. Path must be relative to \code{Dropbox root}.
+#' @param  file_to_write Specifies the path to the local file you want to write.
+#' @param ...  additional arguments passed on to \code{\link{dropbox_get}}
+#' @export
+#' @seealso dropbox_get
+#' @return NULL
+#' @examples \dontrun{
+#' db.get.xlsx(db_cred, 'dropbox_folder/file.xlsx', 'local_folder/file.xlsx')
+#'}
+db.get.xlsx <- function(dropbox_credentials, file_to_get, file_to_write, ...)
+{
+    file <- dropbox_get(dropbox_credentials, file_to_get, ...)
+    attributes(file) <- NULL
+    
+    ftag <- file(file_to_write,'wb')
+    writeBin(file, ftag)
+    close(ftag)
+}
+
